4a2d9f8095a2550d5c8450260d5e2b999ab6058b
wd30gsrc <gd28.c.tateno@gmail.com>
2020-08-08 23:03:35 +0900

    swiper追加

--- components/TheKeyVisualPicture.vue
+++ components/TheKeyVisualPicture.vue
@@ -1,26 +1,51 @@
 <template>
-  <div class="fv__inner__picture">
-    <div class="key-img">
-      <img
-        src="~/assets/imgs/top/slide-02.png"
-        width="500"
-        height="500"
-        alt=""
-      />
-    </div>
-  </div>
+  <swiper
+    ref="mySwiper"
+    :options="swiperOption"
+    class="fv__inner__picture swiper"
+  >
+    <swiper-slide v-for="slideImage in slideImages" :key="slideImage.id">
+      <div class="key-img">
+        <img :src="slideImage.img" width="500" height="500" alt="" />
+      </div>
+    </swiper-slide>
+  </swiper>
 </template>
 
 <script>
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
+
 export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
   data() {
     return {
-      // props: {
-      //   slideImg: {
-      //     type: String,
-      //   },
-      // },
+      slideImages: [
+        { id: 1, img: require('~/assets/imgs/top/slide-02.png') },
+        { id: 2, img: require('~/assets/imgs/top/slide-01.png') },
+        { id: 3, img: require('~/assets/imgs/top/slide-03.png') },
+        { id: 4, img: require('~/assets/imgs/top/slide-04.png') },
+        { id: 5, img: require('~/assets/imgs/top/slide-05.png') },
+        { id: 6, img: require('~/assets/imgs/top/slide-06.png') },
+      ],
+      swiperOption: {
+        spaceBetween: 30,
+        loop: true,
+        effect: 'fade',
+        autoplay: {
+          delay: 6500,
+          disableOnInteraction: false,
+        },
+      },
     }
+  },
+  computed: {
+    swiper() {
+      return this.$refs.mySwiper.$swiper
+    },
   },
 }
 </script>
@@ -29,13 +54,16 @@ export default {
 // Right area
 .fv__inner__picture {
   width: 50%;
+  overflow: hidden;
+}
 
-  .key-img {
-    font-size: 0;
-  }
-  .key-img > img {
-    width: 100%;
-  }
+.key-img {
+  font-size: 0;
+}
+.key-img > img {
+  width: 100%;
+  background: #ffffff;
+  border: 1px solid #ffffff;
 }
 
 @media screen and (max-width: 768px) {
